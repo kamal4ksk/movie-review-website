bc179131accc035903e733910e3c78abbb5f00ba
KAMAL <140543763+kamal4ksk@users.noreply.github.com>
2024-03-11 10:03:18 +0530

    Add files via upload

---
+++ feedback/feedback.js
@@ -0,0 +1,7 @@
+const form = document.querySelector("form");
+Text1 = eField.querySelector("input"),
+textput = pField.querySelector("input");
+email1= form.querySelector(".email"),
+eput= eField.querySelector("input"),
+(form.value=="")? Text1.classList.add("error")
+(form.value=="")? email1.classList.add("error")
